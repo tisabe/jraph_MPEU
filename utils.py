fixed normalization with 1/N

utils.py
@@ -145,12 +145,10 @@ def normalize_targets(inputs, outputs):
     
     if config.AVG_READOUT:
         scaled_targets = outputs
-        mean = np.mean(scaled_targets)
-        std = np.std(scaled_targets)
     else:
         scaled_targets = np.array(outputs)/n_atoms
-        mean = np.sum(scaled_targets) # why sum not mean?? defined like this in paper
-        std = np.sqrt(np.sum(np.square(scaled_targets - mean)))
+    mean = np.mean(scaled_targets)
+    std = np.std(scaled_targets)
 
     if config.AVG_READOUT:
         return (scaled_targets - mean)/std, mean, std

utils_test.py
@@ -215,10 +215,11 @@ class TestHelperFunctions(unittest.TestCase):
         expected_std = 0
         for i in range(n):
             expected_mean += labels[i]/graphs[i].n_node
+        expected_mean = expected_mean/n
         
         for i in range(n):
             expected_std += np.square(labels[i]/graphs[i].n_node - expected_mean)
-        expected_std = np.sqrt(expected_std)
+        expected_std = np.sqrt(expected_std/n)
         
         for i in range(n):
             expected_targets[i] = (labels[i] - graphs[i].n_node*expected_mean)/expected_std