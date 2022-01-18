967a1d823e3635d2486ffd4e43c4c3dddb6dcb6c
Tim Bechtel <tim.bechtel@physik.hu-berlin.de>
2022-01-18 14:32:19 +0100

    work on train.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 '''Config module for global variables. This is the (canonical) way.'''
 
-LABEL_SIZE = None # must define before use, no default
+LABEL_SIZE = 1 # must define before use, no default
 N_HIDDEN_C = 32
 MAX_ATOMIC_NUMBER = 100
 AVG_MESSAGE = False # if the average instead of sum should be used for message aggregation
--- train.py
+++ train.py
@@ -1,9 +1,12 @@
+import os
+from typing import NamedTuple, Callable, Dict, Iterable, Sequence
+
 import logging
 import jax
 import jax.numpy as jnp
+from flax.training import train_state
 import jraph
 import ml_collections
-from typing import NamedTuple, Callable, Dict, Iterable, Sequence
 import numpy as np
 import optax
 import haiku as hk
@@ -14,13 +17,14 @@ from utils import *
 import config as config_globals # TODO: switch from global parameters
 from input_pipeline import get_datasets
 
-
+'''
 class TrainState(NamedTuple):
-    net: hk.Transformed # graph net function
+    apply_fn: hk.Transformed # graph net function
     params: hk.Params # haiku weight parameters
     opt_state: optax.OptState # optax optimizer state
     opt_update: optax.Updates # optax optimizer update
-
+    step: int
+'''
 
 def create_model(config: ml_collections.ConfigDict):
     '''Return a function that applies the graph model.'''
@@ -31,7 +35,7 @@ def create_model(config: ml_collections.ConfigDict):
     config_globals.NUM_MP_LAYERS = config.message_passing_steps
     config_globals.AVG_MESSAGE = config.avg_aggregation_message
     config_globals.AVG_READOUT = config.avg_aggregation_readout
-    return net_nf
+    return net_fn
 
 
 def create_optimizer(
@@ -49,8 +53,8 @@ def create_optimizer(
 
 @jax.jit
 def train_step(
-    state: TrainState, graphs: jraph.GraphsTuple
-) -> Tuple[TrainState, float]:
+    state: train_state.TrainState, graphs: jraph.GraphsTuple
+) -> Tuple[train_state.TrainState, float]:
     '''Perform one update step over the batch of graphs. 
     Returns a new TrainState and the loss MAE over the batch.'''
     return state, 0.0
@@ -58,13 +62,13 @@ def train_step(
 
 @jax.jit
 def evaluate_step(
-    state: TrainState, graphs: jraph.GraphsTuple
+    state: train_state.TrainState, graphs: jraph.GraphsTuple
 ) -> float:
     return 0.0
 
 
 def evaluate_model(
-    state: TrainState,
+    state: train_state.TrainState,
     datasets: Dict[str, Sequence[jraph.GraphsTuple]],
     splits: Iterable[str]
 ) -> Dict[str, float]:
@@ -74,9 +78,50 @@ def evaluate_model(
 def train_and_evaluate(
     config: ml_collections.ConfigDict,
     workdir: str
-) -> TrainState:
+) -> train_state.TrainState:
     # Get datasets, organized by split.
     logging.info('Loading datasets.')
+    datasets = get_datasets(config)
+
+    # Create and initialize network.
+    logging.info('Initializing network.')
+    rng = jax.random.PRNGKey(42)
+    rng, init_rng = jax.random.split(rng)
+    init_graphs = next(datasets['train'])
+    init_graphs = replace_globals(init_graphs)
+    #print(init_graphs)
+    net_fn = create_model(config)
+    net = hk.without_apply_rng(hk.transform(net_fn))
+    params = net.init(rng, init_graphs) # create weights etc. for the model
+
+    # Create the optimizer
+    tx = create_optimizer(config)
+    #opt_init, opt_update = create_optimizer(config)
+    #opt_state = opt_init(params)
+
+    # Create the training state
+    state = train_state.TrainState.create(
+        apply_fn=net.apply, params=params, tx=tx)
+    
+    # Set up checkpointing of the model.
+    checkpoint_dir = os.path.join(workdir, 'checkpoints')
+    # start at step 1
+    initial_step = int(state.step) + 1
+    # TODO: get some framework for automatic checkpoint restoring
+
+    # Begin training loop.
+    logging.info('Starting training.')
+    for step in range(initial_step, config.num_train_steps_max + 1):
+
+        # Split PRNG key, to ensure different 'randomness' for every step.
+        rng, dropout_rng = jax.random.split(rng)
+
+        # Perform a training step
+        graphs = next(datasets['train'])
+        state, loss = train_step(state, graphs)
+
+
+
 
     return 0
     
