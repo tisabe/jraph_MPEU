67d63e8420726f46ae5dfef029fd2be6813207db
Tim Bechtel <tim.bechtel@physik.hu-berlin.de>
2022-02-15 18:56:44 +0100

    added knearest to asedb_to_graphs

--- scripts/asedb_to_graphs.py
+++ scripts/asedb_to_graphs.py
@@ -18,11 +18,11 @@ def get_graph_cutoff(atoms: Atoms, cutoff):
     nodes = [] # initialize arrays, to be filled in loop later
     senders = []
     receivers = []
-    connections_offset = []
     edges = []
     atom_numbers = atoms.get_atomic_numbers() # get array of atomic numbers
 
-    # divide cutoff by 2, because ASE defines two atoms as neighbours when their spheres of radii r overlap
+    # divide cutoff by 2, because ASE defines two atoms as neighbours 
+    # when their spheres of radii r overlap
     radii = [cutoff/2] * len(atoms) # make a list with length len(atoms)
     neighborhood = NeighborList(
         radii, skin=0.0, self_interaction=False, bothways=True
@@ -49,7 +49,6 @@ def get_graph_cutoff(atoms: Atoms, cutoff):
 
             senders.append(jj)
             receivers.append(ii)
-            connections_offset.append(np.vstack((offs, np.zeros(3, float))))
             edges.append(dist)
 
     if len(edges) == 0:
@@ -64,6 +63,83 @@ def get_graph_cutoff(atoms: Atoms, cutoff):
         np.array(receivers)
     )
 
+def get_graph_knearest(
+    atoms: Atoms, num_neighbors, initial_radius=3.0
+):
+    '''Return the graph features, with knearest adjacency.
+    Inspired by https://github.com/peterbjorgensen/msgnet/blob/master/src/msgnet/dataloader.py'''
+
+    atoms.wrap() # put atoms inside unit cell by wrapping their positions
+    atom_numbers = atoms.get_atomic_numbers()
+    unitcell = atoms.get_cell()
+
+    # We want to calculate k nearest neighbors, so we start within a sphere with radius R.
+    # In this sphere we are calculating the number of neighbors, if there are not enough,
+    # i.e. the number of neighbors within the sphere is smaller than k, R is increased 
+    # until we found enough neighbors. After that we discard all neighbors except the k nearest. 
+    for multiplier in range(1, 11):
+        if multiplier == 10:
+            raise RuntimeError("Reached maximum radius")
+        radii = [initial_radius * multiplier] * len(atoms)
+        neighborhood = NeighborList(
+            radii, skin=0.0, self_interaction=False, bothways=True
+        )
+        neighborhood.update(atoms)
+
+        nodes = []
+        edges = []
+        senders = []
+        receivers = []
+        if np.any(atoms.get_pbc()):
+            atom_positions = atoms.get_positions(wrap=True)
+        else:
+            atom_positions = atoms.get_positions(wrap=False)
+        keep_edges = []
+        keep_senders = []
+        keep_receivers = []
+
+        for ii in range(len(atoms)):
+            nodes.append(atom_numbers[ii])
+
+        early_exit = False
+        for ii in range(len(atoms)):
+            this_edges = []
+            this_senders = []
+            this_receivers = []
+            neighbor_indices, offset = neighborhood.get_neighbors(ii)
+            if len(neighbor_indices) < num_neighbors:
+                # Not enough neigbors, so exit and increase radius
+                early_exit = True
+                break
+            for jj, offs in zip(neighbor_indices, offset):
+                ii_pos = atom_positions[ii]
+                jj_pos = atom_positions[jj] + np.dot(offs, unitcell)
+                dist_vec = ii_pos - jj_pos
+                dist = np.sqrt(np.dot(dist_vec, dist_vec))
+
+                this_edges.append([dist])
+                this_senders.append(jj)
+                this_receivers.append(ii)
+            edges.append(np.array(this_edges))
+            senders.append(np.array(this_senders))
+            receivers.append(np.array(this_receivers))
+        if early_exit:
+            continue
+        else:
+            for e, s, r in zip(edges, senders, receivers):
+                # Keep only num_neighbors closest indices
+                keep_ind = np.argsort(e[:, 0])[0:num_neighbors]
+                keep_edges.append(e[keep_ind])
+                keep_senders.append(s[keep_ind])
+                keep_receivers.append(r[keep_ind])
+        break
+    return (
+        np.array(nodes),
+        atom_positions,
+        np.concatenate(keep_edges).flatten(),
+        np.concatenate(keep_senders),
+        np.concatenate(keep_receivers),
+    )
 
 
 def main(args):
@@ -81,10 +157,16 @@ def main(args):
 
             # calculate adjacency of graph as senders and receivers
             cutoff = args.cutoff
-            nodes, atom_positions, edges, senders, receivers = get_graph_cutoff(atoms, cutoff)
+            if args.cutoff_type == 'const':
+                nodes, atom_positions, edges, senders, receivers = get_graph_cutoff(atoms, cutoff)
+            elif args.cutoff_type == 'knearest':
+                cutoff = int(cutoff)
+                nodes, atom_positions, edges, senders, receivers = get_graph_knearest(atoms, cutoff)
+            else:
+                raise ValueError(f'Cutoff type {args.cutoff_type} not recognised.')
             data = {'senders': senders, 'receivers': receivers, 'edges': edges}
             # add information about cutoff
-            prop_dict['cutoff_type'] = 'const'
+            prop_dict['cutoff_type'] = args.cutoff_type
             prop_dict['cutoff_val'] = cutoff
 
             # save in new database
@@ -102,7 +184,9 @@ if __name__ == "__main__":
                         help='input file name')
     parser.add_argument('-o', type=str, dest='file_out', required=True,
                         help='output file name')
+    parser.add_argument('-cutoff_type', type=str, dest='cutoff_type', required=True,
+                        help='choose the cutoff type, knearest or const')            
     parser.add_argument('-cutoff', type=float, dest='cutoff', default=4.0,
-                        help='output file name')
+                        help='cutoff distance or number of nearest neighbors')
     args = parser.parse_args()
     main(args)
