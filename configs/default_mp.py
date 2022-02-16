2750cc1ad67c0c493899a34bc6259bb416e9426a
Tim Bechtel <tim.bechtel@physik.hu-berlin.de>
2022-02-16 14:00:39 +0100

    added config inheritance

--- configs/default_mp.py
+++ configs/default_mp.py
@@ -3,29 +3,13 @@
 '''
 
 import ml_collections
+from configs import default as cfg
 
 def get_config() -> ml_collections.ConfigDict():
     '''Get hyperparameter configuration. Returns a ml_collections.ConfigDict() object.
     '''
-    config = ml_collections.ConfigDict()
+    config = cfg.get_config() # inherit from default config
     
-    # Optimizer
-    config.optimizer = 'adam'
-    config.schedule = 'exponential_decay'
-    config.init_lr = 5e-4 # initial learning rate
-    # parameters for exponential schedule
-    config.transition_steps = 100_000
-    config.decay_rate = 0.96
-
-    config.loss_type = 'MSE'
-
-    # Training hyperparameters
-    config.batch_size = 32
-    config.num_train_steps_max = 10_000_000
-    config.log_every_steps = 10_000
-    config.eval_every_steps = 50_000
-    config.early_stopping_steps = 1_000_000
-    config.checkpoint_every_steps = 50_000
     # data split settings
     config.data_file = 'matproj/mp_graphs_knn.db'
     config.label_str = 'delta_e' # string to determine which label is used from the dataset
@@ -34,22 +18,13 @@ def get_config() -> ml_collections.ConfigDict():
 
     # data selection parameters
     config.selection = 'fold>=0'
-    config.limit_data = None
-
+    
     # MPNN hyperparameters
-    config.message_passing_steps = 3
     config.latent_size = 256
-    config.hk_init = None
     config.max_input_feature_size = 100
     config.aggregation_message_type = 'mean'
     config.aggregation_readout_type = 'mean'
-    # Edge embedding parameters
-    config.k_max = 150
-    config.delta = 0.1
-    config.mu_min = 0.0
     # Node embedding parameters
     config.max_atomic_number = 90
     
-    # Logging options
-    config.log_to_file = False # if logging should go to file if true or console if false
     return config
