75d36d04fad48fd560f06c03e1c7b7b2764c0266
Tim Bechtel <tim.bechtel@physik.hu-berlin.de>
2022-02-24 15:42:09 +0100

    set2set

--- models.py
+++ models.py
@@ -5,12 +5,48 @@ import numpy as np
 import haiku as hk
 import ml_collections
 
+from typing import (
+    Optional
+)
+
 
 # define the shifted softplus activation function
 LOG2 = jnp.log(2)
 def shifted_softplus(x: jnp.ndarray) -> jnp.ndarray:
     # continuous version of relu activation function
     return jnp.logaddexp(x, 0) - LOG2
+
+
+def get_set2set_fn(batch_size, latent_size, num_passes):
+    '''Return the set2set function. It has the same call signature as 
+    jraph.segment_sum, so it can be used as aggregation function 
+    in a jraph.GraphNetwork.'''
+    lstm = hk.LSTM((batch_size, latent_size))
+    q_star = jnp.zeros((batch_size, latent_size))
+
+    lstm_init = lstm.initial_state(batch_size)
+    next_state = lstm_init
+
+    def set2set(data: jnp.ndarray,
+        segment_ids: jnp.ndarray,
+        num_segments: Optional[int] = None,
+        indices_are_sorted: bool = False,
+        unique_indices: bool = False
+    ):
+        m_i = data
+        for i in range(num_passes):
+            q, next_state = lstm(q_star, next_state)
+            e_i = jnp.sum(jnp.multiply(m_i, q), axis=-1)
+            a_i = jraph.segment_softmax(e_i, 
+                segment_ids, num_segments, 
+                indices_are_sorted, unique_indices)
+            r = jraph.segment_sum(
+                a_i * m_i, segment_ids, num_segments, 
+                indices_are_sorted, unique_indices)
+            q_star = jnp.concatenate((q, r), axis=-1)
+
+        return q_star
+    return set2set
 
 
 def get_edge_embedding_fn(latent_size, k_max, delta, mu_min):
