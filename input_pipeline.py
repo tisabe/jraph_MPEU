71921f1cb9500a11e5741963f4eb22c323e75e17
dts <dts@physik.hu-berlin.de>
2022-03-31 21:09:33 +0200

    Cleanup syntax/comments/docstrings in input_pipeline.py

--- input_pipeline.py
+++ input_pipeline.py
@@ -1,3 +1,10 @@
+"""Functions to import data and engineer data into expected graph format.
+
+The functions in this file interface with the ase.db to make lists of jraph
+graphs, where the labels are standardized and the nodes have the right numbers 
+as input features.
+"""
+
 from xmlrpc.client import Boolean
 import ml_collections
 from absl import logging
@@ -15,40 +22,36 @@ from utils import (
     normalize_targets_config,
     add_labels_to_graphs,
 )
-"""
-The functions in this file interface with the ase.db to make lists of jraph graphs,
-where the labels are standardized and the nodes have the right numbers 
-as input features.
-"""
 
-def ase_row_to_jraph(row: ase.db.row.AtomsRow
-) -> jraph.GraphsTuple:
-    '''Return the ase row as a graph.'''
+
+def ase_row_to_jraph(row: ase.db.row.AtomsRow) -> jraph.GraphsTuple:
+    """Return the ASE row as a graph."""
     senders = row.data['senders']
     receivers = row.data['receivers']
     edges = row.data['edges']
     atoms = row.toatoms()
     nodes = atoms.get_atomic_numbers()
-    
+
     graph = jraph.GraphsTuple(
         n_node=np.asarray([len(nodes)]),
         n_edge=np.asarray([len(senders)]),
         nodes=nodes, edges=edges,
         globals=None,
         senders=np.asarray(senders), receivers=np.asarray(receivers))
-    
+
     return graph
 
-def asedb_to_graphslist(file: str, label_str: str, 
-    selection: str = None, limit=None
-) -> Tuple[Sequence[jraph.GraphsTuple], list]:
-    '''Return a list of graphs, by loading rows from local ase database at file.'''
+def asedb_to_graphslist(
+        file: str, label_str: str, 
+        selection: str = None, limit=None) -> Tuple[
+            Sequence[jraph.GraphsTuple], list]:
+    """Return a list of graphs, by loading rows from local ase database at file."""
     graphs = []
     labels = []
     db = ase.db.connect(file)
     count = 0
     #print(f'Selection: {selection}')
-    for i, row in enumerate(db.select(selection=selection, limit=limit)):
+    for _, row in enumerate(db.select(selection=selection, limit=limit)):
         graph = ase_row_to_jraph(row)
         if len(graph.edges) == 0: # do not include graphs without edges
             continue
@@ -56,14 +59,17 @@ def asedb_to_graphslist(file: str, label_str: str,
         label = row.key_value_pairs[label_str]
         labels.append(label)
         count += 1
-    
+
     return graphs, labels
 
-def atoms_to_nodes_list(graphs: Sequence[jraph.GraphsTuple]
-) -> Tuple[Sequence[jraph.GraphsTuple], int]:
-    '''Return graphs with atomic numbers as graph-nodes turned into 
-    nodes with atomic numbers as classes. This gets rid of atomic numbers that are
-    not present in the dataset.
+def atoms_to_nodes_list(graphs: Sequence[jraph.GraphsTuple]) -> Tuple[
+        Sequence[jraph.GraphsTuple], int]:
+    """Encodes the atomic numbers of nodes in a graph in compact fashion.
+    
+    Return graphs with atomic numbers as graph-nodes turned into
+    nodes with atomic numbers as classes. This gets rid of atomic numbers that
+    are not present in the dataset.
+
     Example: atomic numbers as nodes before:
     [1 1 1 1 6] Methane
     [1 1 1 1 1 1 6 6] Ethane
@@ -73,39 +79,45 @@ def atoms_to_nodes_list(graphs: Sequence[jraph.GraphsTuple]
     [0 0 0 0 0 0 1 1]
     [0 0 0 0 1 2]
     as there are only three different atomic numbers present in the list.
-    Also return the number of classes.'''
-    num_list = [] # list with atomic numbers in the graphs list
-    # generate full list first
-    for graph in graphs:
-        nodes = graph.nodes
-        for num in nodes:
+    Also return the number of classes."""
+    num_list = [] # List with atomic numbers in the graphs list.
+    # Generate full list first.
+    for graph in graphs:  # Loop over all graphs.
+        nodes = graph.nodes  # Grab information about nodes in the graph.
+        for num in nodes:  # Loop over nodes in a graph.
             if not num in num_list:
+                # Append unseen atomic numbers to num_list.
                 num_list.append(num)
-    # transform atomic numbers into classes
+    # Transform atomic numbers into classes. Meaning relabel the atomic number
+    # compactly with a new compact numbering system.
     for graph in graphs:
         nodes = graph.nodes
         for i, num in enumerate(nodes):
             nodes[i] = num_list.index(num)
         graph._replace(nodes=nodes)
-            
+
     return graphs, len(num_list)
 
+
 class DataReader:
-    def __init__(self, data: Sequence[jraph.GraphsTuple], 
-    batch_size: int, repeat: Boolean, seed: int = None):
-        self.data = data[:] # pass a copy of the list
+    """Data reader class."""
+    def __init__(
+            self, data: Sequence[jraph.GraphsTuple],
+            batch_size: int, repeat: Boolean, seed: int = None):
+        self.data = data[:]  # Pass a copy of the list.
         self.batch_size = batch_size
         self.repeat = repeat
         self.total_num_graphs = len(data)
         self.seed = seed
         self._generator = self._make_generator()
-        
-        self.budget = estimate_padding_budget_for_batch_size(self.data, batch_size,
+
+        self.budget = estimate_padding_budget_for_batch_size(
+            self.data, batch_size,
             num_estimation_graphs=100)
 
-        # this makes this thing complicated. From outside of DataReader
+        # This makes this thing complicated. From outside of DataReader
         # we interface with this batch generator, but this batch_generator
-        # needs an iterator itself which is also defined in this class
+        # needs an iterator itself which is also defined in this class.
         self.batch_generator = jraph.dynamically_batch(
             self._generator, 
             self.budget.n_node,
@@ -119,11 +131,11 @@ class DataReader:
         return next(self.batch_generator)
 
     def _reset(self):
-        # reset the generator object
-        # does not work 
+        """Reset the generator object."""
+        # TODO(Tim): Right now it does not work.
         self._generator = self._make_generator()
         self.batch_generator = jraph.dynamically_batch(
-            self._generator, 
+            self._generator,
             self.budget.n_node,
             self.budget.n_edge,
             self.budget.n_graph)
@@ -137,14 +149,16 @@ class DataReader:
             if not self.repeat:
                 if idx == self.total_num_graphs:
                     idx = 0
-                    '''here's the problem. At the end of iterating through this dataset,
-                    this return is reached and when trying to iterate with this generator
-                    again, only one None is returned'''
+                    # Here's the problem. At the end of iterating through this
+                    # dataset, this return is reached and when trying to
+                    # iterate with this generator again, only one None is
+                    # returned.
                     return
             else:
                 if idx == self.total_num_graphs:
                     random.shuffle(self.data)
-                # This will reset the index to 0 if we are at the end of the dataset.
+                # This will reset the index to 0 if we are at the end of the
+                # dataset.
                 idx = idx % self.total_num_graphs
             graph = self.data[idx]
             idx += 1
@@ -152,58 +166,81 @@ class DataReader:
 
 
 def get_datasets(config: ml_collections.ConfigDict) -> Tuple[
-        Dict[str, Iterable[jraph.GraphsTuple]], Dict[str, Sequence[jraph.GraphsTuple]], float, float]:
-    """Return a dict with a dataset for each split (training, validation, testing),
-    in normalized and in raw form/labels. Also return the mean and standard deviation.
+        Dict[str, Iterable[jraph.GraphsTuple]],
+        Dict[str, Sequence[jraph.GraphsTuple]],
+        float, float]:
+    """Return a dict with a dataset for each split (train, val, test).
+
+    Return in normalized and in raw form/labels. Also return the mean and
+    standard deviation.
 
     Each dataset is an iterator that yields batches of graphs.
-    The training dataset will reshuffle each time the end of the list has been 
+    The training dataset will reshuffle each time the end of the list has been
     reached, while the validation and test sets are only iterated over once.
 
     The raw dataset is just a dict with lists of graphs.
 
     The graphs have their regression label as a global feature attached.
     """
-    # data will be split into normaized data for regression and raw data for analyzing later
-    graphs_list, labels_list = asedb_to_graphslist(config.data_file, 
-        label_str=config.label_str, selection=config.selection,
+    # Data will be split into normaized data for regression and raw data for
+    # analyzing later
+    graphs_list, labels_list = asedb_to_graphslist(
+        config.data_file,
+        label_str=config.label_str,
+        selection=config.selection,
         limit=config.limit_data)
-    # convert the atomic numbers in nodes to classes and set number of classes
+    # Convert the atomic numbers in nodes to classes and set number of classes.
     graphs_list, num_classes = atoms_to_nodes_list(graphs_list)
     config.max_atomic_number = num_classes
     labels_raw = labels_list
-    
-    labels_list, mean, std = normalize_targets_config(graphs_list, labels_list, config)
+
+    labels_list, mean, std = normalize_targets_config(
+        graphs_list, labels_list, config)
     logging.info(f'Mean: {mean}, Std: {std}')
     graphs_list = add_labels_to_graphs(graphs_list, labels_list)
     graphs_raw = add_labels_to_graphs(graphs_list, labels_raw)
-    
 
-    # split the graphs into three splits using the fractions defined in config
-    train_set, val_and_test_set, train_raw, val_and_test_raw = sklearn.model_selection.train_test_split(
-        graphs_list, graphs_raw,
-        test_size=config.test_frac+config.val_frac,
-        random_state=0
-    )
-    val_set, test_set, val_raw, test_raw = sklearn.model_selection.train_test_split(
-        val_and_test_set, val_and_test_raw,
-        test_size=config.test_frac/(config.test_frac+config.val_frac),
-        random_state=1
-    )
-    
-    # define iterators and generators
-    reader_train = DataReader(data=train_set, 
-        batch_size=config.batch_size, repeat=True, seed=config.seed)
-    reader_val = DataReader(data=val_set, 
-        batch_size=config.batch_size, repeat=False)
-    reader_test = DataReader(data=test_set, 
-        batch_size=config.batch_size, repeat=False)
+    # Split the graphs into three splits using the fractions defined in config.
+    (
+        train_set,
+        val_and_test_set,
+        train_raw,
+        val_and_test_raw) = sklearn.model_selection.train_test_split(
+            graphs_list, graphs_raw,
+            test_size=config.test_frac+config.val_frac,
+            random_state=0)
 
-    dataset = {'train': reader_train, 
+    (
+        val_set,
+        test_set,
+        val_raw,
+        test_raw) = sklearn.model_selection.train_test_split(
+            val_and_test_set, val_and_test_raw,
+            test_size=config.test_frac/(config.test_frac+config.val_frac),
+            random_state=1)
+
+    # Define iterators and generators.
+    reader_train = DataReader(
+        data=train_set, 
+        batch_size=config.batch_size,
+        repeat=True,
+        seed=config.seed)
+    reader_val = DataReader(
+        data=val_set, 
+        batch_size=config.batch_size,
+        repeat=False)
+    reader_test = DataReader(
+        data=test_set, 
+        batch_size=config.batch_size,
+        repeat=False)
+
+    dataset = {
+        'train': reader_train, 
         'validation': reader_val, 
         'test': reader_test}
 
-    dataset_raw = {'train': train_raw, 
+    dataset_raw = {
+        'train': train_raw, 
         'validation': val_raw, 
         'test': test_raw}
 
