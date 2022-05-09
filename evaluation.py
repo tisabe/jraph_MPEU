f208bbce408312fb93cef5206c48db34332bc49b
Tim Bechtel <tim.bechtel@physik.hu-berlin.de>
2022-05-09 15:50:03 +0200

    add saving of inferences

--- evaluation.py
+++ evaluation.py
@@ -7,7 +7,9 @@ the model can be built using the config.json.
 
 import pickle
 import argparse
+import os
 
+from collections import defaultdict
 from absl import logging
 import haiku as hk
 import matplotlib.pyplot as plt
@@ -68,25 +70,62 @@ def get_predictions(dataset, net, params):
     return preds
 
 
+def load_inference_file(workdir, redo=False):
+    """Return the inferences of the model and data defined in workdir.
+
+    This function finds inferences that have already been saved in the working
+    directory. If a file with inferences has been found, they are loaded and
+    returned in a dictionary with splits as keys.
+    If there is no file with inferences in workdir or 'redo' is true, the model
+    is loaded and inferences are calculated.
+    """
+    inference_dict = {}
+    path = workdir + '/inferences.pkl'
+    if not os.path.exists(path) or redo:
+        # compute the inferences
+        logging.info('Loading model.')
+        net, params = load_model(workdir)
+        logging.info('Loading datasets.')
+        dataset, _, mean, std = load_data(workdir)
+        splits = dataset.keys()
+        print(splits)
+
+        for split in splits:
+            data_list = dataset[split].data
+            logging.info(f'Predicting {split} data.')
+            preds = get_predictions(data_list, net, params)
+            targets = [graph.globals[0] for graph in data_list]
+            # scale the predictions and targets using the std
+            preds = preds*float(std) + mean
+            targets = np.array(targets)*float(std) + mean
+        
+            inference_dict[split] = {}
+            inference_dict[split]['preds'] = preds
+            inference_dict[split]['targets'] = targets
+
+        with open(path, 'wb') as inference_file:
+            pickle.dump(inference_dict, inference_file)
+    else:
+        # load inferences from dict
+        logging.info('Loading existing inference.')
+        with open(path, 'rb') as inference_file:
+            inference_dict = pickle.load(inference_file)
+    return inference_dict
+
+
 def main(args):
+    logging.set_verbosity(logging.INFO)
     workdir = args.folder
-    print('Loading model.')
-    net, params = load_model(workdir)
-    print('Loading datasets.')
-    dataset, dataset_raw, mean, std = load_data(workdir)
-    splits = dataset.keys()
+    
+    inference_dict = load_inference_file(workdir, redo=args.redo)
 
     fig, ax = plt.subplots(2)
     marker_size = 0.3
 
+    splits = inference_dict.keys()
     for split in splits:
-        data_list = dataset[split].data
-        print(f'Predicting {split} data.')
-        preds = get_predictions(data_list, net, params)
-        targets = [graph.globals[0] for graph in data_list]
-        # scale the predictions and targets using the std
-        preds = preds*float(std) + mean
-        targets = np.array(targets)*float(std) + mean
+        preds = inference_dict[split]['preds']
+        targets = inference_dict[split]['targets']
         error = np.abs(preds - targets)
         mae = np.mean(error)
         mse = np.mean(np.square(error))
@@ -97,13 +136,14 @@ def main(args):
         ax[0].scatter(targets, preds, s=marker_size, label=label_string)
         ax[1].scatter(targets, error, s=marker_size, label=split)
 
+    units = input("Type units of prediction and target: ")
     # plot x = y regression lines
     x = np.linspace(*ax[0].get_xlim())
     ax[0].plot(x, x, '--', alpha=0.2, color='grey')
     ax[0].set_title('Model regression performance')
-    ax[0].set_ylabel('prediction (eV)', fontsize=12)
-    ax[1].set_xlabel('target (eV)', fontsize=12)
-    ax[1].set_ylabel('error (eV)', fontsize=12)
+    ax[0].set_ylabel(f'prediction ({units})', fontsize=12)
+    ax[1].set_xlabel(f'target ({units})', fontsize=12)
+    ax[1].set_ylabel(f'error ({units})', fontsize=12)
     ax[0].legend()
     ax[1].legend()
     ax[1].set_yscale('log')
@@ -120,5 +160,8 @@ if __name__ == "__main__":
     parser.add_argument(
         '-f', '-F', type=str, dest='folder', default='results/qm9/test_eval',
         help='input directory name')
+    parser.add_argument(
+        '--redo', dest='redo', action='store_true'
+    )
     args = parser.parse_args()
     main(args)
