8ed138631c18296a3dd89574cee7eab6f07dd8cc
Tim Bechtel <bechtelt@physik.hu-berlin.de>
2022-06-23 15:41:16 +0200

    added plots to error_analysis

--- scripts/plotting/error_analysis.py
+++ scripts/plotting/error_analysis.py
@@ -24,6 +24,7 @@ def main(argv):
         raise app.UsageError('Too many command-line arguments.')
     workdir = FLAGS.file
     df_path = workdir + '/result.csv'
+    config = load_config(workdir)
 
     if not os.path.exists(df_path) or FLAGS.redo:
         logging.info('Did not find csv path, generating DataFrame.')
@@ -35,10 +36,88 @@ def main(argv):
         logging.info('Found csv path. Reading DataFrame.')
         df = pd.read_csv(df_path)
 
+    df['abs. error'] = abs(df['prediction'] - df[config.label_str])
+    # get dataframe with only test data
+    df_test = df.loc[lambda df_temp: df_temp['split']=='test']
+
     fig, ax = plt.subplots()
-    sns.scatterplot(x='enthalpy_formation_atom', y='prediction', data=df, hue='split', ax=ax)
+    sns.scatterplot(
+        x=config.label_str,  # plot prediction vs label
+        y='prediction',
+        data=df,
+        hue='split',
+        ax=ax
+    )
     plt.show()
-    fig.savefig(workdir+'/error_analysis.png', bbox_inches='tight', dpi=600)
+    fig.savefig(workdir+'/pred_vs_label.png', bbox_inches='tight', dpi=600)
+
+    fig, ax = plt.subplots()
+    sns.scatterplot(
+        x=config.label_str, # plot error vs label
+        y='abs. error',
+        data=df,
+        hue='split',
+        ax=ax
+    )
+    plt.show()
+    fig.savefig(workdir+'/error_vs_label.png', bbox_inches='tight', dpi=600)
+
+    fig, ax = plt.subplots()
+    sns.boxplot(
+        x='dft_type', # plot error vs dft type
+        y='abs. error',
+        data=df,
+        hue='split',
+        ax=ax
+    )
+    plt.yscale('log')
+    plt.show()
+    fig.savefig(workdir+'/error_vs_label.png', bbox_inches='tight', dpi=600)
+    
+    # group the spacegoups into crystal systems
+    #df['spacegroup_relax'] = df['spacegroup_relax'].astype('category')
+    bins = [0, 2, 15, 74, 142, 167, 194, 230]
+    labels = [
+        'Triclinic', 'Monoclinic', 'Orthorhombic', 'Tetragonal',
+        'Trigonal', 'Hexagonal', 'Cubic']
+    df['crystal system'] = pd.cut(df['spacegroup_relax'], bins, labels=labels)
+    fig, ax = plt.subplots()
+    sns.boxplot(
+        x='crystal system', # plot error vs space group
+        y='abs. error',
+        data=df,
+        hue='split',
+        ax=ax
+    )
+    plt.axhline(y=1e-2, color='black', linestyle='--')
+    plt.yscale('log')
+    plt.show()
+    fig.savefig(workdir+'/error_vs_label.png', bbox_inches='tight', dpi=600)
+
+    fig, ax = plt.subplots()
+    sns.boxplot(
+        x='Egap_type', # plot error vs Egap type
+        y='abs. error',
+        data=df,
+        hue='split',
+        ax=ax
+    )
+    plt.axhline(y=1e-2, color='black', linestyle='--')
+    plt.yscale('log')
+    plt.show()
+    fig.savefig(workdir+'/error_vs_label.png', bbox_inches='tight', dpi=600)
+
+    fig, ax = plt.subplots()
+    sns.scatterplot(
+        x='density', # plot error vs density
+        y='abs. error',
+        data=df,
+        hue='split',
+        ax=ax
+    )
+    plt.yscale('log')
+    plt.show()
+    fig.savefig(workdir+'/error_vs_label.png', bbox_inches='tight', dpi=600)
 
 
 if __name__ == "__main__":
