3e1f7b0086f8d8701b31a643f82400a8732f4b2d
Tim Bechtel <bechtelt@physik.hu-berlin.de>
2022-07-05 16:41:57 +0200

    add and fix tests for utils

--- jraph_MPEU/utils.py
+++ jraph_MPEU/utils.py
@@ -109,6 +109,22 @@ def dist_matrix(position_matrix):
     return jnp.sqrt(distance_matrix)
 
 
+def normalize_targets_dict(graphs_dict, labels_dict, aggregation_type):
+    """Wrapper for normalize targets, when graphs and labels are dicts."""
+    # convert from dicts to arrays
+    inputs = list(graphs_dict.values())
+    outputs = list(labels_dict.values())
+
+    res, mean, std = normalize_targets(inputs, outputs, aggregation_type)
+
+    # convert results back into dict
+    res_dict = {}
+    for id_single, label in zip(labels_dict.keys(), res):
+        res_dict[id_single] = label
+
+    return res_dict, mean, std
+
+
 def normalize_targets(inputs, outputs, aggregation_type):
     """Return normalized outputs, based on aggregation type.
 
@@ -131,40 +147,6 @@ def normalize_targets(inputs, outputs, aggregation_type):
         return (outputs - (mean*n_atoms))/std, mean, std
     else:
         return (scaled_targets - mean)/std, mean, std
-
-
-def normalize_targets_dict(graphs_dict, labels_dict, aggregation_type):
-    """Return normalized outputs, based on aggregation type.
-
-    Also return mean and standard deviation for later rescaling.
-    Inputs, i.e. graphs, are used to get number of atoms,
-    for atom-wise scaling."""
-    # convert from dicts to arrays
-    inputs = list(graphs_dict.values())
-    outputs = list(labels_dict.values())
-    outputs = np.reshape(outputs, len(outputs)) # convert to 1-D array
-    n_atoms = np.zeros(len(outputs)) # save all numbers of atoms in array
-    for i in range(len(outputs)):
-        n_atoms[i] = inputs[i].n_node[0]
-
-    if aggregation_type == 'sum':
-        scaled_targets = np.array(outputs)/n_atoms
-    else:
-        scaled_targets = outputs
-    mean = np.mean(scaled_targets)
-    std = np.std(scaled_targets)
-
-    if aggregation_type == 'sum':
-        res = (outputs - (mean*n_atoms))/std
-    else:
-        res = (scaled_targets - mean)/std
-
-    # convert results back into dict
-    res_dict = {}
-    for id_single, label in zip(labels_dict.keys(), res):
-        res_dict[id_single] = label
-
-    return res_dict, mean, std
 
 
 def scale_targets(inputs, outputs, mean, std, aggregation_type):
