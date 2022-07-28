ac09e671d5fbf4d197af502f8371bbea2fdb05ed
Tim Bechtel <bechtelt@physik.hu-berlin.de>
2022-07-28 19:40:39 +0200

    added new script for aflow pull

---
+++ scripts/data/get_aflow_csv_pages.py
@@ -0,0 +1,39 @@
+"""Pulling Data from AFLOW."""
+
+import json, sys, os
+from urllib.request import urlopen
+import pandas
+
+# Find all properties in http://aflow.org/API/aflux/?schema
+
+SERVER = "http://aflow.org"
+API = "/API/aflux/v1.0/?"
+MATCHBOOK = (
+    'enthalpy_formation_atom(*),'
+    'dft_type(*),ldau_type(*),species_pp_ZVAL(*),energy_cutoff(*),'
+    'energy_atom(*),density(*),'#volume_cell(*),'
+    'geometry_orig,positions_cartesian,compound' # geometry parameters needed for unit cell
+    )
+print("URL:", SERVER+API+MATCHBOOK)
+
+i = 35
+while i < 50:
+    print("Page: " + str(i))
+    DIRECTIVES = f'$paging({int(i)},10000)'
+    summons = MATCHBOOK+","+DIRECTIVES
+    
+    response = urlopen(SERVER+API+summons)
+    print("Response code:" + str(response.code))
+    data = response.read()
+    data = json.loads(data)
+    print(type(data))
+
+    df = pandas.DataFrame(data)
+    if df.empty:
+        break
+    else:
+        i += 1
+
+"""if input("Save the dataset? [y/n]") == "y":
+    df.to_csv(
+        (input('Type directory and filename as "dir/filename.csv": ')))"""
