1495e1ad60e54230795b190842bf4a5e0a6a3a0e
Tim Bechtel <bechtelt@physik.hu-berlin.de>
2022-11-16 16:44:35 +0100

    improved analysis in classify.py and new script to generate split file with predicted insulators

--- scripts/plotting/classify.py
+++ scripts/plotting/classify.py
@@ -13,7 +13,7 @@ import sklearn.metrics
 from sklearn.metrics import RocCurveDisplay
 from sklearn.metrics import roc_auc_score
 
-from jraph_MPEU.utils import load_config, str_to_list
+from jraph_MPEU.utils import str_to_list
 from jraph_MPEU.inference import get_results_df
 from jraph_MPEU.input_pipeline import cut_egap
 
@@ -27,14 +27,14 @@ flags.DEFINE_integer('tick_size', 12, 'font size to use in labels')
 
 
 def main(argv):
-    """Get the model inferences and plot regression."""
+    """Get the model inferences and plot classification probabilities and
+    scores like accuracy and ROC-AUC on the test set."""
     logging.set_verbosity(logging.INFO)
     if len(argv) > 1:
         raise app.UsageError('Too many command-line arguments.')
 
     workdir = FLAGS.file
     df_path = workdir + '/result.csv'
-    config = load_config(workdir)
 
     if not os.path.exists(df_path) or FLAGS.redo:
         logging.info('Did not find csv path, generating DataFrame.')
@@ -49,24 +49,19 @@ def main(argv):
 
     df['class_true'] = df['Egap'].apply(cut_egap) #  1 is insulator, 0 is metal
     df['p_insulator'] = 1 - df['prediction']
+    # calculate the class prediction by applying a threshold. Because of the
+    # softmax outputs probability, the threshold is exactly 1/2
     df['class_pred'] = df['p_insulator'].apply(lambda p: (p > 0.5)*1)
-    #print(df[['class_true', 'p_insulator']])
+    df['class_correct'] = df['class_true'] == df['class_pred']
+    print(df[df['class_correct'] == False][[
+        'class_true', 'class_pred', 'class_correct']])
     df_test = df.loc[lambda df_temp: df_temp['split'] == 'test']
-
-    plt.hist(df_test['p_insulator'], bins=100, log=True)
-    #plt.show()
-    # calculate binary cross entropy
-    #bce = -1 * np.array(df['class_true']) * np.log(df['p_insulator']) \
-    #    - (1 - np.array(df['class_true'])) * np.log(1 - np.array(df['p_insulator']))
-    #plt.hist(bce)
-
-    print(df_test['class_pred'][df_test['class_pred'] == 1])
 
     # calculate and display ROC curve
     y_pred = df_test['p_insulator'].to_numpy().reshape(-1, 1)
     y_true = df_test['class_true'].to_numpy()
     fig, ax = plt.subplots()
-    display = RocCurveDisplay.from_predictions(y_true, y_pred, ax=ax)
+    _ = RocCurveDisplay.from_predictions(y_true, y_pred, ax=ax)
     x_ref = np.linspace(*ax.get_xlim())
     ax.plot(x_ref, x_ref, '--', alpha=0.2, color='grey')
     ax.set_xlabel('False positive rate', fontsize=FLAGS.font_size)
@@ -84,6 +79,27 @@ def main(argv):
     acc = sklearn.metrics.accuracy_score(
         df_test['class_true'], df_test['class_pred'])
     print(f"Total accuracy on test set: {acc}")
+
+    df_correct = df[df['class_correct'] == True]
+    df_incorrect = df[df['class_correct'] == False]
+    plt.hist(df_correct['p_insulator'], bins=100, log=True)
+    plt.hist(df_incorrect['p_insulator'], bins=100, log=True)
+    plt.xlabel(r'$\hat{p}_{insulator}$')
+    plt.ylabel('count')
+    #plt.show()
+    #sns.histplot(df_test, x='p_insulator', hue='class_correct', log_scale=False)
+    plt.show()
+
+    counts_correct, bins_correct = np.histogram(df['p_insulator'], bins=100)
+    counts_incorrect, _ = np.histogram(df_incorrect['p_insulator'], bins=100)
+    plt.stairs(counts_incorrect/counts_correct, bins_correct, fill=True)
+    plt.show()
+
+    # plot distribution of band gaps of predicted insulators
+    df_insulators_test = df_test[df_test['class_true'] == 1]
+    print(df_insulators_test)
+    sns.histplot(df_insulators_test, x='Egap', bins=100)
+    plt.show()
 
 if __name__ == "__main__":
     app.run(main)
