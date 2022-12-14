8d9b733ba55daa42d3c1c0c4ec9711f0f9eaf251
daniel speckhard <speckhard@fhi.mpg.de>
2022-12-14 14:54:51 +0100

    added test for empty dataset

--- tests/test_db.py
+++ tests/test_db.py
@@ -24,6 +24,10 @@ flags.DEFINE_integer(
 
 class UnitTests(absltest.TestCase):
     """Unit test class. This string only exists to make my linter happy."""
+    def test_not_empty(self):
+        """Test that the database is not empty."""
+        database = ase.db.connect(FLAGS.file)
+        self.assertTrue(len(database) > 0)
     def test_vis_and_print(self):
         """Test db by visualizing and printing database entries."""
         database = ase.db.connect(FLAGS.file)
