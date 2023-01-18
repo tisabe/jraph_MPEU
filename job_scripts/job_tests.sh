2b99fc44041604265fbc9f597dfc0850756fbba8
Tim Bechtel <bechtelt@physik.hu-berlin.de>
2023-01-18 13:31:25 +0100

    added job script for tests and changed log file location

--- job_scripts/job_tests.sh
+++ job_scripts/job_tests.sh
@@ -1,23 +1,21 @@
 #!/bin/bash -l
-# set the working directory as a variable:
-# workdir='./results/aflow/test/run2'
 # Standard output and error:
-#SBATCH -o ./output_slurm/job.%j.out
-#SBATCH -e ./output_slurm/job.%j.err
+#SBATCH -o ./output_slurm/test.%j.out
+#SBATCH -e ./output_slurm/test.%j.err
 # Initial working directory:
 #SBATCH -D ./
 # Job name
-#SBATCH -J aflow
+#SBATCH -J tests
 #
 #SBATCH --nodes=1            # Request 1 or more full nodes
 #SBATCH --constraint="gpu"   # Request a GPU node
 #SBATCH --gres=gpu:a100:1    # Use one a100 GPU
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=4
 #SBATCH --ntasks-per-core=1
-#SBATCH --mem=32000        # Request 32 GB of main memory per node in MB units.
+#SBATCH --mem=8        # Request 32 GB of main memory per node in MB units.
 #SBATCH --mail-type=none
 #SBATCH --mail-user=userid@example.mpg.de
-#SBATCH --time=23:00:00
+#SBATCH --time=00:10:00
 
 # load the environment with modules and python packages
 cd ~/envs ; source ~/envs/activate_jax.sh
@@ -25,5 +23,4 @@ cd ~/jraph_MPEU
 
 export OMP_NUM_THREADS=10
 
-python scripts/main.py --workdir=./results/aflow/classify_0 \
---config=jraph_MPEU_configs/aflow_classify.py
+python tests/test_models.py
