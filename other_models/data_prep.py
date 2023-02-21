cb6addd33f81113fda3dc2b2dc38b9193b2a3eeb
dts <speckhard@fhi.mpg.de>
2023-02-21 19:31:30 +0100

    add a few tests for feature engineering for ElemNet and ElemRF.

--- other_models/data_prep.py
+++ other_models/data_prep.py
@@ -3,6 +3,7 @@
 Here we fed an ASE atomic objects and we need to return atomic information
 about the elements in the ASE atoms object."""
 
+import numpy as np
 import pandas as pd
 
 
@@ -18,18 +19,57 @@ class DataPrep():
         """
         if functional == 'pbe':
             self.elemental_features_csv = (
-                'other_models/elemnet/really_tight_full_cut20_revpbe.csv')
+                'other_models/really_tight_full_cut20_revpbe.csv')
         else:
             raise ValueError('functional can only be PBE currently.')
 
         self.ase_db_path = ase_db_path
         self.pbe_features_df = pd.read_csv(self.elemental_features_csv)
 
-    def get_elemental_data_of_atom(self, atomic_number):
-        ea_half = float(self.pbe_features_df[
-            self.pbe_features_df['Atomic number'] == 1]['EA_half'])
-        return ea_half
+    def get_csv_val(self, atomic_number, feature):
+        """Get elemental data from elemental data CSV for a given element."""
+        csv_val = float(self.pbe_features_df[
+            self.pbe_features_df['Atomic number'] == atomic_number][feature])
+        return csv_val
 
+
+    def get_feature_bar_val(
+            self, atomic_number_list, fraction_list, feature):
+        """"Get the feature average result for a given compound."""
+        feature_val_list = np.zeros(len(atomic_number_list))
+        # Loop over every atomic number in our list and get the relevant
+        # value from the CSV for a given feature. We specify in our CSV
+        # the atomic number and the feature and this gives us a spefic cell
+        # value of our CSV. The atomic numbers are the rows and the features
+        # are the columns.
+        for i, atomic_number in enumerate(atomic_number_list):
+            feature_val_list[i] = self.get_csv_val(
+                atomic_number, feature)
+        # The dot product gives us our summation equation from the paper.
+        # We are getting the sum of f_i*x_i over i where f_i is the feature val
+        # and x_i is the fraction value.
+        return np.dot(feature_val_list, fraction_list), feature_val_list
+
+    def get_feature_hat_val(
+            self, fraction_list, feature_val_list, f_bar):
+        """"Get the feature average deviation result for a given compound.
+        
+        This function should be called after get_feature_bar_val has been called
+        since we want to feed in the feature average value (f_bar) and the
+        feature_val_list.
+        """
+        feature_hat = 0
+        for i in range(len(fraction_list)):
+            feature_hat += fraction_list[i]*np.abs(feature_val_list[i] - f_bar)
+        return feature_hat
+
+    def get_features_row(compound_name, features_list):
+        """Get the row of CSV data for a features list for a given compound."""
+        # We need to get f_hat and f_bar for each feature in our feature list.
+        # This is why we initialize the row to be twice as big as the number
+        # of features.
+        features_row = np.zeros(2*len(features_list))
+        for i, feature in enumerate(features_list):
 
 
 
--- tests/test_elemnet_data_prep.py
+++ tests/test_elemnet_data_prep.py
@@ -2,6 +2,7 @@
 
 from absl.testing import absltest
 from absl import logging
+import numpy as np
 
 import sys
 import os
@@ -15,18 +16,42 @@ import other_models.data_prep as dp
 
 class UnitTests(absltest.TestCase):
     """Unit test class. This string only exists to make my linter happy."""
-    def test_get_elemental_data_of_atom(self):
-        """Test get elemental data from csv works."""
 
-        data_prep_obj = dp.DataPrep(
+    def setUp(self):
+        self.data_prep_obj = dp.DataPrep(
             ase_db_path='/None',
             functional='pbe')
+
+    def test_get_get_csv_val(self):
+        """Test getting elemental data from csv works."""
+
         atomic_number = 1
+        feature = 'EA_half'
         expected_ea_half = 1.05595894
-        data_prep_ea_half = data_prep_obj.get_elemental_data_of_atom(
-            atomic_number)
+        data_prep_ea_half = self.data_prep_obj.get_csv_val(
+            atomic_number, feature)
         self.assertEqual(expected_ea_half, data_prep_ea_half)
 
+    def test_get_feature_bar_val(self):
+        atomic_number_list = np.array([26, 8])
+        fraction_list = np.array([0.4, 0.6])
+        feature = 'Atomic number'
+        expected_average_atomic_number = 15.2
+        average_atomic_number, feature_val_list = self.data_prep_obj.get_feature_bar_val(
+            atomic_number_list=atomic_number_list,
+            fraction_list=fraction_list, feature=feature)
+        self.assertEqual(average_atomic_number, expected_average_atomic_number)
+
+
+    def test_get_feature_hat_val(self):
+        fraction_list = np.array([0.4, 0.6])
+        f_bar = 15.2
+        feature_val_list = np.array([26, 8])
+        expected_deviation_atomic_number = 8.64
+        average_deviation_atomic_number = self.data_prep_obj.get_feature_hat_val(
+            fraction_list=fraction_list, feature_val_list=feature_val_list, f_bar=f_bar)
+        self.assertEqual(
+            average_deviation_atomic_number, expected_deviation_atomic_number)
 
 if __name__ == "__main__":
     absltest.main()
