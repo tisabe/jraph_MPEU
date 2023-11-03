e8e2d8dab9f80ee5ae42260dd9ad6620717dc28e
Tim Bechtel <bechtelt@physik.hu-berlin.de>
2023-11-03 14:24:38 +0100

    made minimum working example of graphProcessor

--- jraph_MPEU/normalization.py
+++ jraph_MPEU/normalization.py
@@ -1,23 +1,100 @@
-"""This module defines the classes and functions for normalizing and scaling of
-input data. The API is meant to resemble functions in sklearn.preprocessing."""
+"""This module defines the classes and functions for transformation of input
+data. The API is meant to resemble functions in sklearn.preprocessing."""
 
 from typing import List
+import pickle
+from collections import defaultdict
+
 import jraph
+import sklearn.preprocessing as skp
 
 
-class GraphNormalizer:
-    """Class to calculate and apply normalization to graph structured data."""
-    def __init__(self, workdir=None):
-        pass
+TRANSFORMS = {
+    'scalar_intrinsic': skp.StandardScaler,
+    'one_hot': skp.OneHotEncoder,
+}
+
+
+def init_new_transformer(input: List, transform_name: str):
+    transformer = TRANSFORMS[transform_name]()
+    return transformer
+
+
+class GraphPreprocessor:
+    """Class to calculate and apply transformation to graph structured data."""
+    def __init__(self, property_dict, config, workdir=None):
+        """property_dict contains information about the graph data to be
+        transformed. The keys at level 0 are 'globals', 'nodes', 'edges'.
+        At level 1, keys are the names of the properties (one or multiple).
+
+        Here is an example for a property_dict:
+        property_dict = {
+            'globals': {
+                'Egap': 'scalar_intrinsic',
+                'spacegroup': 'one_hot'
+            },
+            'nodes': {
+                'atomic_number': 'element_list'
+            },
+            'edges': {
+                'distance': 'rbf',
+                'bond': 'one_hot'
+            }
+        }
+        
+        If workdir argument is passed, automatically saves parameters."""
+        if workdir is not None:
+            self._autosave = True
+        else:
+            self._autosave = False
+        self._workdir = workdir
+        self.property_dict = dict(property_dict) # create a copy
+
+    def save_parameters(self):
+        raise NotImplementedError
 
     def fit(self, graphs: List[jraph.GraphsTuple]):
-        raise NotImplementedError
+        graph_values_dict = {
+            'globals': {},
+            'nodes': {},
+            'edges': {}
+        }
+        for graph in graphs:
+            # check that the graphs are not batched
+            #assert(type(graph.n_node[0])==int)
+            for prop_name, value in graph.globals.items():
+                if not prop_name in graph_values_dict['globals']:
+                    graph_values_dict['globals'][prop_name] = []
+                graph_values_dict['globals'][prop_name].append(value)
 
-    def fit_transform(self, graphs: List[jraph.GraphsTuple]):
-        raise NotImplementedError
+            for prop_name, value in graph.nodes.items():
+                if not prop_name in graph_values_dict['nodes']:
+                    graph_values_dict['nodes'][prop_name] = []
+                graph_values_dict['nodes'][prop_name].append(value)
+            
+            for prop_name, value in graph.edges.items():
+                if not prop_name in graph_values_dict['edges']:
+                    graph_values_dict['edges'][prop_name] = []
+                graph_values_dict['edges'][prop_name].append(value)
+
+        print(graph_values_dict)
+
+        for feature, feature_dict in self.property_dict.items():
+            # feature is e.g. 'globals'
+            for prop_name, prop_transform in feature_dict.items():
+                # prop_name is e.g. 'Egap', prop_transform is e.g. 'scalar'
+                transform = TRANSFORMS[prop_transform]()
+                self.property_dict[feature][prop_name] = transform
+                transform.fit(X=graph_values_dict[feature][prop_name])
+
+        return self.property_dict
 
     def transform(self, graphs: List[jraph.GraphsTuple]):
         raise NotImplementedError
+
+    def fit_transform(self, graphs: List[jraph.GraphsTuple]):
+        self.fit(graph)
+        return self.transform(graphs)
 
     def inverse_transform(self, graphs: List[jraph.GraphsTuple]):
         raise NotImplementedError
---
+++ tests/test_normalization.py
@@ -0,0 +1,67 @@
+"""Test the functions of the input_pipeline_test.py module."""
+
+import tempfile
+import unittest
+
+import numpy as np
+import jraph
+
+from jraph_MPEU.normalization import GraphPreprocessor
+
+
+class Unittest(unittest.TestCase):
+    """Testing class."""
+    def setUp(self):
+        """Set up default test case."""
+        graph0 = jraph.GraphsTuple(
+            n_node=np.array([4]), nodes={'feature': np.array([0,0,2,2])},
+            n_edge=np.array([4]), edges={'feature': np.array([3,3,4,4])},
+            senders=np.array([0,1,2,3]), receivers=np.array([0,1,2,3]),
+            globals={'categorical_feature': np.array([0]),
+            'scalar_feature': np.array([0])})
+        graph1 = jraph.GraphsTuple(
+            n_node=np.array([6]), nodes={'feature': np.array([0,0,2,2,4,4])},
+            n_edge=np.array([6]), edges={'feature': np.array([3,3,4,4,5,5])},
+            senders=np.array([0,1,2,3,4,5]), receivers=np.array([0,1,2,3,4,5]),
+            globals={'categorical_feature': np.array([1]),
+            'scalar_feature': np.array([1])})
+        self.graphs_default = [graph0, graph1]
+        self.property_dict_globals = {
+            'globals': {
+                'categorical_feature': 'one_hot',
+                'scalar_feature': 'scalar_intrinsic'
+            }
+        }
+        self.property_dict_default = {
+            'globals': {
+                'feature': 'one_hot'
+            },
+            'nodes': {
+                'feature': 'scalar_intrinsic'
+            },
+            'edges': {
+                'feature': 'scalar_intrinsic'
+            }
+        }
+
+    def test_GraphPreprocessor(self):
+        config = {}
+        processor = GraphPreprocessor(self.property_dict_globals, config)
+        transform_dict = processor.fit(self.graphs_default)
+        print(transform_dict)
+        for prop_name, transform in transform_dict['globals'].items():
+            print(transform.get_params())
+        print(transform_dict['globals']['scalar_feature'].mean_)
+        print(transform_dict['globals']['categorical_feature'].categories_)
+
+    def test_batched_graphs(self):
+        """Test that an error is raised, if there are batched graphs in list."""
+        graphs_batched = jraph.batch(self.graphs_default)
+        list_batched = [graphs_batched, graphs_batched]
+        config = {}
+        processor = GraphPreprocessor(self.property_dict_globals, config)
+        self.assertRaises(AssertionError, processor.fit(list_batched))
+
+
+if __name__ == '__main__':
+    unittest.main()
