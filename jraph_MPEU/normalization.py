cb0eca215508b146d0b0da4142ed057a99ae18bc
Tim Bechtel <bechtelt@physik.hu-berlin.de>
2023-11-09 15:44:05 +0100

    finished test of ExtrinsicScalarEncoder

--- jraph_MPEU/normalization.py
+++ jraph_MPEU/normalization.py
@@ -14,7 +14,7 @@ class ElementEncoder:
     """Class to encode lists of elements. Specifically for node vectors
     representing elements or atomic numbers."""
     def __init__(self):
-        self.enc = skp.OneHotEncoder(sparse_output=False)
+        self.enc = skp.OneHotEncoder(sparse=False)
 
     def fit(self, element_list: List[np.array]):
         flattened_list = np.hstack(element_list) # stacks and flattens arrays
@@ -29,15 +29,90 @@ class ElementEncoder:
         return transformed_list
 
     def fit_transform(self, element_list: List[np.array])->List[np.array]:
-        pass
+        self.fit(element_list)
+        return self.transform(element_list)
+
+    def inverse_transform(self, one_hot_list: List[np.array])->List[np.array]:
+        raise NotImplementedError
+
+
+class ExtrinsicScalarEncoder:
+    """Class to encode lists of extrinsic scalars.
+    
+    Extrinsic scalars, as opposed to intrinsic scalars, scale proportionally
+    with the number of nodes in a graph. I.e. total energy might be an example
+    of an extrinsic scalar of an atomic graph."""
+    def __init__(self):
+        self.enc = skp.StandardScaler()
+
+    def fit(self, X: List[np.array], n_nodes: List[int]):
+        """Fit encoder using features X and n_node in n_nodes."""
+        scaled_X = []
+        for feature, n_node in zip(X, n_nodes):
+            scaled_X.append(feature / n_node)
+        self.enc.fit(scaled_X)
+
+    def transform(self, X: List[np.array], n_nodes: List[int]):
+        """Return transformed X using n_node in n_nodes."""
+        std = self.enc.scale_
+        mean = self.enc.mean_
+        return [(feature - n_node*mean)/std for feature, n_node in zip(X, n_nodes)]
+
+    def fit_transform(self, X: List[np.array], n_nodes: List[int]):
+        self.fit(X, n_nodes)
+        return self.transform(X, n_nodes)
+
+    def inverse_transform(self, X_tr: List[np.array], n_nodes: List[int]):
+        """Scale back the data to the original representation, using n_node in n_nodes."""
+        std = self.enc.scale_
+        mean = self.enc.mean_
+        return [feature_tr*std + n_node*mean for feature_tr, n_node in zip(X_tr, n_nodes)]
 
 
 TRANSFORMS = {
     'scalar_intrinsic': skp.StandardScaler,
     'one_hot': skp.OneHotEncoder,
-    'element_list': ElementEncoder
+    'element_list': ElementEncoder,
+    'scalar_extrinsic': ExtrinsicScalarEncoder
 }
+TRANSFORMS_THAT_TAKE_N_NODE = (
+    'scalar_extrinsic'
+)
 
+
+def collect_graph_values_dict(graphs: List[jraph.GraphsTuple]):
+    """Returns a dict with graph properties in lists."""
+    graph_values_dict = {
+        'globals': {},
+        'nodes': {},
+        'edges': {},
+        'n_nodes': [],
+        'n_edges': []
+    }
+    
+    for graph in graphs:
+        # check that the graphs are not batched
+        assert(len(graph.n_node)==1)
+        graph_values_dict['n_nodes'].append(graph.n_node[0])
+        graph_values_dict['n_edges'].append(graph.n_edge[0])
+        # TODO: the next block repeats three times, could be shorter
+        # TODO: maybe change to looping over property_dict, could be faster
+        for prop_name, value in graph.globals.items():
+            if not prop_name in graph_values_dict['globals']:
+                graph_values_dict['globals'][prop_name] = []
+            graph_values_dict['globals'][prop_name].append(value)
+
+        for prop_name, value in graph.nodes.items():
+            if not prop_name in graph_values_dict['nodes']:
+                graph_values_dict['nodes'][prop_name] = []
+            graph_values_dict['nodes'][prop_name].append(value)
+        
+        for prop_name, value in graph.edges.items():
+            if not prop_name in graph_values_dict['edges']:
+                graph_values_dict['edges'][prop_name] = []
+            graph_values_dict['edges'][prop_name].append(value)
+    return graph_values_dict
+        
 
 class GraphPreprocessor:
     """Class to calculate and apply transformation to graph structured data."""
@@ -67,46 +142,29 @@ class GraphPreprocessor:
         else:
             self._autosave = False
         self._workdir = workdir
-        self.property_dict = dict(property_dict) # create a copy
+        self.property_dict = dict(property_dict) # copy to loop over
+        self.transforms_dict = dict(property_dict) # copy to re-populate values
 
     def save_parameters(self):
         raise NotImplementedError
 
     def fit(self, graphs: List[jraph.GraphsTuple]):
-        graph_values_dict = {
-            'globals': {},
-            'nodes': {},
-            'edges': {}
-        }
-        for graph in graphs:
-            # check that the graphs are not batched
-            #assert(type(graph.n_node[0])==int)
-            for prop_name, value in graph.globals.items():
-                if not prop_name in graph_values_dict['globals']:
-                    graph_values_dict['globals'][prop_name] = []
-                graph_values_dict['globals'][prop_name].append(value)
-
-            for prop_name, value in graph.nodes.items():
-                if not prop_name in graph_values_dict['nodes']:
-                    graph_values_dict['nodes'][prop_name] = []
-                graph_values_dict['nodes'][prop_name].append(value)
-            
-            for prop_name, value in graph.edges.items():
-                if not prop_name in graph_values_dict['edges']:
-                    graph_values_dict['edges'][prop_name] = []
-                graph_values_dict['edges'][prop_name].append(value)
-
-        print(graph_values_dict)
+        graph_values_dict = collect_graph_values_dict(graphs)
 
         for feature, feature_dict in self.property_dict.items():
             # feature is e.g. 'globals'
             for prop_name, prop_transform in feature_dict.items():
                 # prop_name is e.g. 'Egap', prop_transform is e.g. 'scalar'
                 transform = TRANSFORMS[prop_transform]()
-                self.property_dict[feature][prop_name] = transform
-                transform.fit(X=graph_values_dict[feature][prop_name])
+                self.transforms_dict[feature][prop_name] = transform
+                if prop_transform in TRANSFORMS_THAT_TAKE_N_NODE:
+                    transform.fit(
+                        X=graph_values_dict[feature][prop_name],
+                        n_nodes=graph_values_dict['n_nodes'])
+                else:
+                    transform.fit(X=graph_values_dict[feature][prop_name])
 
-        return self.property_dict
+        return self.transforms_dict
 
     def transform(self, graphs: List[jraph.GraphsTuple]):
         raise NotImplementedError
--- tests/test_normalization.py
+++ tests/test_normalization.py
@@ -5,8 +5,10 @@ import unittest
 
 import numpy as np
 import jraph
+import sklearn.preprocessing as skp
 
-from jraph_MPEU.normalization import GraphPreprocessor, ElementEncoder
+from jraph_MPEU.normalization import (
+    GraphPreprocessor, ElementEncoder, ExtrinsicScalarEncoder)
 
 
 class Unittest(unittest.TestCase):
@@ -44,6 +46,42 @@ class Unittest(unittest.TestCase):
             }
         }
 
+    def test_ExtrinsicScalarEncoder(self):
+        features = [
+            np.random.randint(0, 10, 4),
+            np.random.randint(0, 10, 4)]
+        n_nodes = np.random.randint(1, 10, len(features))
+
+        features_per_atom = [
+            feature/n_node for feature, n_node in zip(features, n_nodes)]
+        mean_expected = sum(features_per_atom)/len(features_per_atom)
+
+        enc = ExtrinsicScalarEncoder()
+        enc.fit(features, n_nodes)
+        np.testing.assert_array_equal(mean_expected, enc.enc.mean_)
+        diff_from_mean = [
+            feature_per_atom - mean_expected for feature_per_atom in features_per_atom]
+        sq_diffs = [np.square(diff) for diff in diff_from_mean]
+        std_expected = np.sqrt(sum(sq_diffs)/len(sq_diffs))
+        # for zero variance, sklearn puts scale to 1, so this is how we test it
+        std_expected[std_expected == 0] = 1.
+        np.testing.assert_array_equal(std_expected, enc.enc.scale_)
+
+        features_tr = enc.transform(features, n_nodes)
+        for feature_tr, feature, n_node in zip(features_tr, features, n_nodes):
+            np.testing.assert_array_equal(
+                feature_tr, (feature - n_node*mean_expected)/std_expected)
+
+        features_fit_tr = enc.fit_transform(features, n_nodes)
+        for feature_tr, feature, n_node in zip(features_fit_tr, features, n_nodes):
+            np.testing.assert_array_equal(
+                feature_tr, (feature - n_node*mean_expected)/std_expected)
+
+        features_inverse = enc.inverse_transform(features_tr, n_nodes)
+        for feature_inverse, feature in zip(features_inverse, features):
+            np.testing.assert_array_equal(feature_inverse, feature)
+
+
     def test_ElementEncoder(self):
         element_list = [
             np.array([8,1,1]), # H2O
@@ -51,17 +89,40 @@ class Unittest(unittest.TestCase):
         encoder = ElementEncoder()
         encoder.fit(element_list)
         transformed_list = encoder.transform(element_list)
-        print(transformed_list)
+        np.testing.assert_array_equal(
+            transformed_list[0], np.array([[0,0,1],[1,0,0],[1,0,0]]))
+        np.testing.assert_array_equal(
+            transformed_list[1],
+            np.array([[0,1,0],[1,0,0],[1,0,0],[1,0,0],[1,0,0]]))
 
-    def test_GraphPreprocessor(self):
+        element_list = [
+            np.array(['H','H','O']), # H2O
+            np.array(['C','H','H','H','H'])] # Methane
+        encoder = ElementEncoder()
+        encoder.fit(element_list)
+        transformed_list = encoder.transform(element_list)
+        np.testing.assert_array_equal(
+            transformed_list[0], np.array([[0,1,0],[0,1,0],[0,0,1]]))
+        np.testing.assert_array_equal(
+            transformed_list[1],
+            np.array([[1,0,0],[0,1,0],[0,1,0],[0,1,0],[0,1,0]]))
+
+    def test_GraphPreprocessor_globals(self):
         config = {}
         processor = GraphPreprocessor(self.property_dict_globals, config)
         transform_dict = processor.fit(self.graphs_default)
-        print(transform_dict)
-        for prop_name, transform in transform_dict['globals'].items():
-            print(transform.get_params())
-        print(transform_dict['globals']['scalar_feature'].mean_)
-        print(transform_dict['globals']['categorical_feature'].categories_)
+        #print(transform_dict)
+        #for prop_name, transform in transform_dict['globals'].items():
+        #    print(transform.get_params())
+        self.assertIsInstance(
+            transform_dict['globals']['scalar_feature'], skp.StandardScaler)
+        self.assertEqual(
+            transform_dict['globals']['scalar_feature'].mean_, 0.5)
+        self.assertIsInstance(
+            transform_dict['globals']['categorical_feature'],
+            skp.OneHotEncoder)
+        self.assertEqual(
+            len(transform_dict['globals']['categorical_feature'].categories_[0]),2)
 
     def test_batched_graphs(self):
         """Test that an error is raised, if there are batched graphs in list."""
@@ -69,7 +130,8 @@ class Unittest(unittest.TestCase):
         list_batched = [graphs_batched, graphs_batched]
         config = {}
         processor = GraphPreprocessor(self.property_dict_globals, config)
-        self.assertRaises(AssertionError, processor.fit(list_batched))
+        with self.assertRaises(AssertionError):
+            processor.fit(list_batched)
 
 
 if __name__ == '__main__':
