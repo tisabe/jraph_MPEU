972965591c515ae8bf05dee53b38627b63fc82aa
dts <speckhard@fhi.mpg.de>
2024-01-30 22:45:49 +0100

    pad to nearest multiple of 64.

--- jraph_MPEU/utils.py
+++ jraph_MPEU/utils.py
@@ -220,6 +220,34 @@ def _nearest_bigger_multiple_of_64(num: int) -> int:
     return base
 
 
+def pad_graph_to_nearest_multiple_of_64(
+        graphs_tuple: jraph.GraphsTuple) -> jraph.GraphsTuple:
+    """Pads a batched `GraphsTuple` to the nearest power of two.
+  For example, if a `GraphsTuple` has 7 nodes, 5 edges and 3 graphs, this method
+  would pad the `GraphsTuple` nodes and edges:
+    7 nodes --> 8 nodes (2^3)
+    5 edges --> 8 edges (2^3)
+  And since padding is accomplished using `jraph.pad_with_graphs`, an extra
+  graph and node is added:
+    8 nodes --> 9 nodes
+    3 graphs --> 4 graphs
+  Args:
+    graphs_tuple: a batched `GraphsTuple` (can be batch size 1).
+  Returns:
+    A graphs_tuple batched to the nearest power of two.
+  """
+    # Add 1 since we need at least one padding node for pad_with_graphs.
+    # Note, the plus one should be insid ethe operator since we want a power of
+    # two returned.
+    pad_nodes_to = _nearest_bigger_multiple_of_64(jnp.sum(graphs_tuple.n_node) + 1)
+    pad_edges_to = _nearest_bigger_multiple_of_64(jnp.sum(graphs_tuple.n_edge))
+    # Add 1 since we need at least one padding graph for pad_with_graphs.
+    # We do not pad to nearest power of two because the batch size is fixed.
+    pad_graphs_to = graphs_tuple.n_node.shape[0] + 1
+    return jraph.pad_with_graphs(graphs_tuple, pad_nodes_to, pad_edges_to,
+                                 pad_graphs_to)
+
+
 def pad_graph_to_nearest_power_of_two(
         graphs_tuple: jraph.GraphsTuple) -> jraph.GraphsTuple:
     """Pads a batched `GraphsTuple` to the nearest power of two.
