9e9d9e789de3fb6e6cb5dbe4ae92e4dd67d04c91
Tim Bechtel <bechtelt@physik.hu-berlin.de>
2025-04-01 10:49:38 +0200

    fix loss function and tests

--- jraph_MPEU/loss.py
+++ jraph_MPEU/loss.py
@@ -13,33 +13,26 @@ ArrayTree = Union[jnp.ndarray, Iterable['ArrayTree'], Mapping[Any, 'ArrayTree']]
 def _safe_divide(x, y):
     return jnp.where(y == 0.0, 0.0, x / jnp.where(y == 0.0, 1.0, y))
 
+
 def _safe_mask(graph):
+    """Return the valid graph/node/edge mask, for padded or unpadded graph."""
     mask_dict = {}
-    mask = jraph.get_graph_padding_mask(graph)
-    if not jnp.any(mask):
-        mask_dict['globals'] = [True]*len(graph.n_node)
-    else:
-        mask_dict['globals'] = mask
-
-    mask = jraph.get_node_padding_mask(graph)
-    if not jnp.any(mask):
-        mask_dict['nodes'] = [True]*int(jnp.sum(graph.n_node))
-    else:
-        mask_dict['nodes'] = mask
-
-    mask = jraph.get_edge_padding_mask(graph)
-    if not jnp.any(mask):
-        mask_dict['edges'] = [True]*int(jnp.sum(graph.n_edge))
-    else:
-        mask_dict['edges'] = mask
+    padding_functions = {
+        'globals': jraph.get_graph_padding_mask,
+        'nodes': jraph.get_node_padding_mask,
+        'edges': jraph.get_edge_padding_mask
+    }
+    for key, fun in padding_functions.items():
+        mask = fun(graph) # for unpadded graph, mask is all False
+        mask_dict[key] = jnp.logical_not(jnp.logical_xor(mask, jnp.any(mask)))
     return mask_dict
 
 
 def total_squared_error_pytree(targets: ArrayTree, predictions: ArrayTree) -> float:
     """Returns sum, for mean squared error, divide by number of globals/nodes/edges."""
     diff_sq = jax.tree.map(
-        lambda x, y: float(jnp.sum((x - y)**2)), targets, predictions)
-    return jnp.sum(jax.tree.flatten(diff_sq)[0][0])
+        lambda x, y: jnp.sum((x - y)**2).astype(float), targets, predictions)
+    return sum(jax.tree.flatten(diff_sq)[0]) # TODO: check if this should be jnp.sum
 
 
 class WeightedGlobalsNodesEdgesLoss:
@@ -57,33 +50,35 @@ class WeightedGlobalsNodesEdgesLoss:
     ) -> jnp.ndarray:
         loss = 0
         mask = _safe_mask(predictions)
-        print(mask)
 
         if (self.globals_weight > 0.0
             and targets.globals is not None
             and predictions.globals is not None):
+            n_globals = jnp.sum(mask['globals'])
             loss += self.globals_weight * total_squared_error_pytree(
-                targets.globals, predictions.globals) / jnp.sum(mask['globals'])
+                targets.globals, predictions.globals) / n_globals
 
         if (self.nodes_weight > 0.0
             and targets.nodes is not None
             and predictions.nodes is not None):
+            n_nodes = jnp.sum(mask['nodes'])
             loss += self.nodes_weight * total_squared_error_pytree(
-                targets.nodes, predictions.nodes) / jnp.sum(mask['nodes'])
+                targets.nodes, predictions.nodes) / n_nodes
 
         if (self.edges_weight > 0.0
             and targets.edges is not None
             and predictions.edges is not None):
+            n_edges = jnp.sum(mask['edges'])
             loss += self.edges_weight * total_squared_error_pytree(
-                targets.edges, predictions.edges) / jnp.sum(mask['edges'])
+                targets.edges, predictions.edges) / n_edges
 
         return loss  # [n_graphs, ]
 
     def __repr__(self):
         return (
-            f"{self.__class__.__name__}(energy_weight={self.energy_weight:.3f}, "
-            f"forces_weight={self.forces_weight:.3f}, "
-            f"stress_weight={self.stress_weight:.3f})"
+            f"{self.__class__.__name__}(globals_weight={self.globals_weight:.3f}, "
+            f"nodes_weight={self.nodes_weight:.3f}, "
+            f"edges_weight={self.edges_weight:.3f})"
         )
 
 
--- tests/test_loss.py
+++ tests/test_loss.py
@@ -1,17 +1,22 @@
 """Test loss functions in loss module."""
 
 import numpy as np
+import jax
 import jraph
 
 from jraph_MPEU.loss import WeightedGlobalsNodesEdgesLoss
 
 
-def get_uniform_graph(n_features, factor) -> jraph.GraphsTuple:
-    n_node = np.random.randint(1, 10, (1,))
-    n_edge = np.random.randint(1, 20, (1,))
+def get_uniform_graph(
+    n_node=4, n_edge=8, n_features=10, factor=1, node_feats=True, edge_feats=True
+) -> jraph.GraphsTuple:
+    n_node = np.array([n_node])
+    n_edge = np.array([n_edge])
+    nodes=np.ones((n_node[0], n_features))*factor if node_feats else None
+    edges=np.ones((n_edge[0], n_features))*factor if edge_feats else None
     graph = jraph.GraphsTuple(
-        nodes=np.ones((n_node[0], n_features))*factor,
-        edges=np.ones((n_edge[0], n_features))*factor,
+        nodes=nodes,
+        edges=edges,
         senders=np.random.randint(0, n_node[0], (n_edge[0],)),
         receivers=np.random.randint(0, n_node[0], (n_edge[0],)),
         n_node=n_node,
@@ -21,17 +26,98 @@ def get_uniform_graph(n_features, factor) -> jraph.GraphsTuple:
     return graph
 
 
-def test_WeightedGlobalsNodesEdgesLoss():
+def get_nested_graph(n_node=4, n_edge=8, n_features=10, factor=1) -> jraph.GraphsTuple:
+    n_node = np.array([n_node])
+    n_edge = np.array([n_edge])
+    graph = jraph.GraphsTuple(
+        nodes={
+            'node_key_0': np.ones((n_node[0], n_features))*factor,
+            'node_key_1': np.ones((n_node[0], n_features))*factor
+        },
+        edges={
+            'edge_key_0': np.ones((n_edge[0], n_features))*factor,
+            'edge_subkey': {
+                'edge_key_0': np.ones((n_edge[0], n_features))*factor,
+                'edge_key_1': np.ones((n_edge[0], n_features))*factor
+            }
+        },
+        senders=np.random.randint(0, n_node[0], (n_edge[0],)),
+        receivers=np.random.randint(0, n_node[0], (n_edge[0],)),
+        n_node=n_node,
+        n_edge=n_edge,
+        globals={'test_key': np.ones((n_features,))*factor}
+    )
+    return graph
+
+
+def test_WeightedGlobalsNodesEdgesLoss_zero():
+    """Test graph loss on unpadded graph."""
     loss = WeightedGlobalsNodesEdgesLoss(3, 2, 1)
     graph = get_uniform_graph(10, 1)
-    print(loss(graph, graph))
+    assert loss(graph, graph) == 0
 
-def test_padded_graph():
-    # NOTE: when graph is not padded with jraph.pad_with_graphs,
-    # then number of padding graphs/nodes/edges is equal to
-    # total number of graphs/nodes/edges
-    return
-    
+
+def test_WeightedGlobalsNodesEdgesLoss_zero_pad():
+    """Test graph loss on unpadded graph."""
+    loss = WeightedGlobalsNodesEdgesLoss(3, 2, 1)
+    graph = get_uniform_graph(4, 8, 10, 1, edge_feats=False)
+    graph = jraph.pad_with_graphs(graph, 8, 16)
+    assert loss(graph, graph) == 0
+
+
+def test_WeightedGlobalsNodesEdgesLoss_zero_batch():
+    """Test graph loss on batched graph."""
+    loss = WeightedGlobalsNodesEdgesLoss(3, 2, 1)
+    graph = get_uniform_graph(10, 1)
+    graphs = jraph.batch([graph, graph])
+    assert loss(graphs, graphs) == 0
+
+
+def test_nested_graph_loss():
+    globals_weight, nodes_weight, edges_weight = 1, 1, 1
+    loss = WeightedGlobalsNodesEdgesLoss(
+        globals_weight, nodes_weight, edges_weight
+    )
+    n_node, n_edge, n_feat = 1, 1, 10
+    graph_0 = get_nested_graph(n_node, n_edge, n_feat, factor=1)
+    graph_1 = get_nested_graph(n_node, n_edge, n_feat, factor=2)
+
+    loss_globals = globals_weight * 1 * n_feat
+    loss_nodes = nodes_weight * 2 * n_feat / n_node
+    loss_edges = edges_weight * 3 * n_feat / n_edge
+    loss_total = loss_globals + loss_nodes + loss_edges
+
+    assert loss(graph_0, graph_1) == loss(graph_1, graph_0)
+    assert loss(graph_0, graph_1) == loss_total
+
+    graph_0 = jraph.pad_with_graphs(graph_0, 2, 2)
+    graph_1 = jraph.pad_with_graphs(graph_1, 2, 2)
+
+    assert loss(graph_0, graph_1) == loss(graph_1, graph_0)
+    assert loss(graph_0, graph_1) == loss_total
+
+
+def test_graph_loss_jit():
+    globals_weight, nodes_weight, edges_weight = 1, 1, 1
+    loss = WeightedGlobalsNodesEdgesLoss(
+        globals_weight, nodes_weight, edges_weight
+    )
+    n_node, n_edge, n_feat = 1, 1, 10
+    graph_0 = get_nested_graph(n_node, n_edge, n_feat, factor=1)
+    graph_1 = get_nested_graph(n_node, n_edge, n_feat, factor=2)
+    loss = jax.jit(loss)
+    loss_globals = globals_weight * 1 * n_feat
+    loss_nodes = nodes_weight * 2 * n_feat / n_node
+    loss_edges = edges_weight * 3 * n_feat / n_edge
+    loss_total = loss_globals + loss_nodes + loss_edges
+
+    assert loss(graph_0, graph_1) == loss(graph_1, graph_0)
+    assert loss(graph_0, graph_1) == loss_total
+
 
 if __name__ == "__main__":
-    test_WeightedGlobalsNodesEdgesLoss()
+    test_WeightedGlobalsNodesEdgesLoss_zero()
+    test_WeightedGlobalsNodesEdgesLoss_zero_pad()
+    test_WeightedGlobalsNodesEdgesLoss_zero_batch()
+    test_nested_graph_loss()
+    test_graph_loss_jit()
