7e5a012b552ac1fcd463a18074f323dca47f391c
dts <speckhard@fhi.mpg.de>
2025-09-19 15:39:52 +0200

    update jraph_MPEU

--- jraph_MPEU/train.py
+++ jraph_MPEU/train.py
@@ -628,15 +628,19 @@ def train_and_evaluate(
         logging.info(f'graphs: {graphs}')
         logging.info(f'graphs[0].n_node: {graphs[0].n_node}')
         logging.info(f'[i.n_node for i in graphs]: {[i.n_node for i in graphs]}')
-        graphs = jraph.GraphsTuple(
-            n_node = np.array([i.n_node for i in graphs]),
-            n_edge = np.array([i.n_edge for i in graphs]),
-            nodes = np.array([i.nodes for i in graphs]),
-            edges = np.array([i.edges for i in graphs]),
-            globals = np.array([i.globals for i in graphs]),
-            senders = np.array([i.senders for i in graphs]),
-            receivers = np.array([i.receivers for i in graphs]),
-        )
+        try: 
+            graphs = jraph.GraphsTuple(
+                n_node = np.array([i.n_node for i in graphs]),
+                n_edge = np.array([i.n_edge for i in graphs]),
+                nodes = np.array([i.nodes for i in graphs]),
+                edges = np.array([i.edges for i in graphs]),
+                globals = np.array([i.globals for i in graphs]),
+                senders = np.array([i.senders for i in graphs]),
+                receivers = np.array([i.receivers for i in graphs]),
+            )
+        except:
+            logging.info(f'Failed to get batch for step: {step}')
+
         # Update the weights after a gradient step and report the
         # state/losses/optimizer gradient. The loss returned here is the loss
         # on a batch not on the full training dataset.
