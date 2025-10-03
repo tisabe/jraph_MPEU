90f4b3050d7aabd68b114d779523f936a8f3cf5b
dts <speckhard@fhi.mpg.de>
2025-10-03 14:18:27 +0200

    adjust the create profile experiments for painn dynamic batching.

--- jraph_MPEU/create_profile_experiments.py
+++ jraph_MPEU/create_profile_experiments.py
@@ -67,11 +67,10 @@ JOB_SCRIPT = """#!/bin/bash -l
 
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 
-# cd /u/dansp/multi_gpu_batching/jraph_MPEU
-# This data won't be PaiNN compliant.
-cd /u/dansp/jraph_MPEU
-source /u/dansp/parallel_gpu_py11/venv/bin/activate
-srun python3.11 /u/dansp/parallel_gpu_py11/jraph_MPEU/scripts/main.py --workdir=<folder_name> --config=<config_name>
+# Right now only painn compliant data.
+/u/dansp/multi_gpu_no_block/jraph_MPEU/
+source /u/dansp/multi_gpu_no_block/no_block_env/bin/activate
+srun python3.11 /u/dansp/multi_gpu_no_block/jraph_MPEU/scripts/train.py --workdir=<folder_name> --config=<config_name>
 """
 
 TEMPLATE_SCHNET_CONFIG = """
