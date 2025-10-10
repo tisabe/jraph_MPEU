c40c1b4d08c58a812a93ee2ac07b64e464e34bd9
dts <speckhard@fhi.mpg.de>
2025-10-10 11:37:25 +0200

    remove tensorflow import.

--- scripts/train.py
+++ scripts/train.py
@@ -11,7 +11,6 @@ from absl import flags
 from absl import logging
 import jax
 from ml_collections import config_flags
-import tensorflow as tf
 
 from jraph_MPEU import train, utils
 
@@ -34,7 +33,7 @@ def main(argv):
 
     # Hide any GPUs from TensorFlow. Otherwise TF might reserve memory and make
     # it unavailable to JAX.
-    tf.config.experimental.set_visible_devices([], 'GPU')
+    # tf.config.experimental.set_visible_devices([], 'GPU')
 
     if not os.path.exists(f'./{FLAGS.workdir}'):
         os.makedirs(f'./{FLAGS.workdir}')
